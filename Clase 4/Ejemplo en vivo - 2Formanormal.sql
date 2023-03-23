97126cd30b6a96f434c18e802143b59f92127ef6
David Bustos Usta <dfbustos@uninorte.edu.co>
2023-03-23 10:56:23 -0300

    23-03-2023

---
+++ Clase 4/Ejemplo en vivo - 2Formanormal.sql
@@ -0,0 +1,24 @@
+select * from dafbustosus_coderhouse.customers;
+
+-- Mover esas columnas a una tabla que contenga la información de contacto de las personas
+ALTER TABLE customers
+DROP COLUMN contact_person;
+
+ALTER TABLE customers
+DROP COLUMN contact_person_role;
+
+ALTER TABLE customers
+DROP COLUMN phone_number;
+
+select * from dafbustosus_coderhouse.customers c 
+
+-- crear la tabla contact_persons con un id respectivo
+CREATE TABLE contact_persons (
+    id              INTEGER PRIMARY KEY,
+    name            VARCHAR(300),
+    role            VARCHAR(300),
+    phone_number    VARCHAR(15)
+);
+
+select * from dafbustosus_coderhouse.contact_persons;
+
